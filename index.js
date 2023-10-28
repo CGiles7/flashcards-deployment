f30de94fab707af2350e3a96d962742b6c05ce09
Callie Giles <anchoredinthecrown@gmail.com>
2023-10-28 14:08:53 -0400

    install cors, require in index.js

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const jsonServer = require("json-server");
+const cors = require("cors"); // Import the 'cors' package.
+
 const server = jsonServer.create();
 
 const { PORT = 8080 } = process.env;
 
+// Use the 'cors' middleware to enable CORS.
+server.use(cors());
+
 server.use(jsonServer.defaults());
 server.use(jsonServer.router("data/db.json"));
 
-server.listen(PORT);
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
