ba67ee1d4fd1e7df52ec761b743c1ae63d80b83d
Callie Giles <anchoredinthecrown@gmail.com>
2023-10-28 14:20:47 -0400

    change port

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-const jsonServer = require("json-server");
-const cors = require("cors"); // Import the 'cors' package.
-
-const server = jsonServer.create();
-
-const { PORT = 8080 } = process.env;
-
-// Use the 'cors' middleware to enable CORS.
-server.use(cors());
-
-server.use(jsonServer.defaults());
-server.use(jsonServer.router("data/db.json"));
-
+const { PORT = 3000 } = process.env;
+// ...
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+// const jsonServer = require("json-server");
+// const cors = require("cors"); // Import the 'cors' package.
+
+// const server = jsonServer.create();
+
+// const { PORT = 8080 } = process.env;
+
+// // Use the 'cors' middleware to enable CORS.
+// server.use(cors());
+
+// server.use(jsonServer.defaults());
+// server.use(jsonServer.router("data/db.json"));
+
+// server.listen(PORT, () => {
+//   console.log(`Server is running on port ${PORT}`);
+// });
