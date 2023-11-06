a244adf3c3801e1957e3e1dcd6ecb5f044a69cae
Callie Giles <anchoredinthecrown@gmail.com>
2023-11-06 12:13:47 -0500

    update

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 const jsonServer = require("json-server");
 const server = jsonServer.create();
+const cors = require("cors"); // Import the CORS middleware
 
 const { PORT = 8080 } = process.env;
 
+server.use(cors()); // Use the CORS middleware to enable CORS
 server.use(jsonServer.defaults());
 server.use(jsonServer.router("data/db.json"));
 
-server.listen(PORT);
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
+
+
+// const jsonServer = require("json-server");
+// const server = jsonServer.create();
+
+// const { PORT = 8080 } = process.env;
+
+// server.use(jsonServer.defaults());
+// server.use(jsonServer.router("data/db.json"));
+
+// server.listen(PORT);
